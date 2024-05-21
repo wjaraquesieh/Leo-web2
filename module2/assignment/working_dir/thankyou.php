fa14ab71398e1de2fbf9a3b8a875d59f2f95266c
DiscoBroccoli <19515DearClem@gmail.com>
2024-05-21 15:28:24 -0500

    feat(assignment2): Add starter file of assignment 2

---
+++ module2/assignment/working_dir/thankyou.php
@@ -0,0 +1,28 @@
+<?php
+
+/*******w******** 
+    
+    Name:
+    Date:
+    Description:
+
+****************/
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>Thanks for your order!</title>
+</head>
+<body>
+    <!-- Remember that alternative syntax is good and html inside php is bad -->
+    
+</body>
+</html>
